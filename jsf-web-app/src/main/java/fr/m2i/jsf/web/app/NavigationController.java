7f258eb6257bc43ba558093fdfb6c1de3a8c6864
TABOUGUIA <tabouguiaraissa@yahoo.fr>
2022-07-29 15:11:26 +0200

    navigation conditionnelle

--- jsf-web-app/src/main/java/fr/m2i/jsf/web/app/NavigationController.java
+++ jsf-web-app/src/main/java/fr/m2i/jsf/web/app/NavigationController.java
@@ -6,21 +6,36 @@ package fr.m2i.jsf.web.app;
 
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 
 
 @ManagedBean(name = "navigationController", eager = true)
 public class NavigationController {
 
-
+    @ManagedProperty(value ="navigationController, eager=tru")
+    private String pageId;
+    
     public NavigationController() {
 
+    }
+    
+    public String goToPage(){
+        if (pageId ==null)
+        {
+            return "home";
+        } 
+        return "pageId";
     }
 
     public String moveToHello(){
         return "hello";
     }
-    
-    public String moveToPage2(){
-        return "page2";
+     public String getPageId() {
+        return pageId;
     }
+
+    public void setPageId(String pageId) {
+        this.pageId = pageId;
+    }
+    
 }
